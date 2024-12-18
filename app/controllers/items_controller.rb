9ed0fd8abb50c1c54721e8355ceb2e8682c63602
ktna1727 <keita9076@gmail.com>
2024-12-18 10:55:59 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :correct_user]
+  before_action :correct_user, only: [:edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -19,13 +21,27 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
 
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def correct_user
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def item_params
