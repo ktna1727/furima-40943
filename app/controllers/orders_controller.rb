f33df748858c2926c01215f5675f5e4dcc2964e0
ktna1727 <keita9076@gmail.com>
2024-12-26 13:31:18 +0900

    決済機能導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,30 +2,40 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item
   def index
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @order_form = OrderForm.new
   end
 
   def create
     @order_form = OrderForm.new(order_params)
     if @order_form.valid?
+      pay_item
       @order_form.save
       redirect_to root_path, notice: '購入が完了しました'
     else
-      render :index, status: :unprocessable_entity
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+      render :new, status: :unprocessable_entity
     end
-
-    redirect_to root_path
   end
 
   private
 
   def order_params
     params.require(:order_form).permit(:post_number, :area_id, :municipalities, :street_address, :building_name, :phone_number)
-          .merge(user_id: current_user.id, item_id: @item.id)
+          .merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_kye = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 
   def correct_user
